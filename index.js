6368813a5077628f38dd061bc6f97a401ed2493f
kamal Jyotwal <kkjyotwal100@gmail.com>
2020-08-31 19:47:49 +0530

    Todo complete and working

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 var btn = document.getElementById("button");
-
 btn.addEventListener("click", addingTask);
+var closebtn = document.getElementsByClassName("cross");
 
 function addingTask() {
     var inputfield = document.getElementById('inputtext');
     if (inputfield.value == 0) {
         alert('You need to write something!')
     } else {
+        console.log("active")
         var newli = document.createElement('li');
+        var span = document.createElement('span')
+        newli.className += "licss";
         newli.innerHTML = inputfield.value;
+        span.innerHTML = "&times;"
+        span.className += "cross";
+        newli.appendChild(span);
         document.getElementById('mytasks').appendChild(newli);
+        // console.log(closebtn.length)
         inputfield.value = "";
+
+        // delete button functionality
+        for (var i = 0; i < closebtn.length; i++) {
+            closebtn[i].onclick = function () {
+                var btncontainer = this.parentElement;
+                btncontainer.style.display = "none";
+            }
+        }
     }
-
-
 };
+
+// toggling on task complete
+document.getElementById("mytasks").addEventListener("click", function (e) {
+    if (e.target && e.target.matches("li")) {
+        e.target.classList.toggle("checked");
+    }
+});
+for (var i = 0; i < closebtn.length; i++) {
+    closebtn[i].onclick = function () {
+        var btncontainer = this.parentElement;
+        btncontainer.style.display = "none";
+    }
+}
